50407079c258ae0b7ed54c695b939378d8ca6046
Gabriel Alonso <galonsov22@fpcoruna.afundacion.org>
2023-10-15 12:31:44 +0200

    Tarea 6 (1.2): Modifico conversor2.php añadiendo el cambio entre pies y metros

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -8,7 +8,11 @@ if (isset($_POST["funidad"])){
 if ($_POST["funidad"] == "pulgada") {
 $v_pulgadas = $_POST["fcantidad"];
 $v_metros = $v_pulgadas * 0.0254;
-echo $v_pulgadas."pulgada(s) = ". $v_metros." metro(s)";
+echo $v_pulgadas." pulgada(s) = ". $v_metros." metro(s)";
+}elseif($_POST["funidad"] == "pies"){
+$v_pies = $_POST["fcantidad"];
+$v_metros = $v_pies * 0.3048;
+echo $v_pies. " pie(s) = ". $v_metros. " metro(s)";
 }else{
 echo"unidad no soportada";
 }
@@ -22,6 +26,8 @@ echo"unidad no soportada";
 
 <input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 <label for="pulgada_input">Pulgada(s)</label><br>
+<input type="radio" id="pie_input" name="funidad" value="pies">
+<label for="pie_input">Pies</label><br>
 <input type="radio" id="otro_input" name="funidad" value="otro">
 <label for="otro_input">Otro</label><br>
 
