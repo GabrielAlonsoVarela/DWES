afd8178e80ae06792dfedf0c096a94049e509a57
GabrielAlonsoVarela <145001357+GabrielAlonsoVarela@users.noreply.github.com>
2023-10-24 12:44:15 +0200

    Update comment.php añado comentarios simples

--- sprint2phpsql/www/mysite/comment.php
+++ sprint2phpsql/www/mysite/comment.php
@@ -4,10 +4,10 @@ $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 <html>
 <body>
 <?php
-
+//Introducimos la id del juego que vamos a comentar y el nuevo comentario en variables
 $juego_id = $_POST['juego_id'];
 $comentario = $_POST['new_comment'];
-
+//Realizamos la insercion en la tabla de comentarios
 $query = "INSERT INTO tComentarios (comentario, usuario_id, juego_id, fecha_comentario) VALUES ('".$comentario."', NULL, ".$juego_id.", now())";
 
 mysqli_query($db,$query) or die('Error');
