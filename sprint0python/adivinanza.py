f7aa3e3e6ddf7c185a850949e4355ba7f2b4641b
GabrielAlonsoVarela <145001357+GabrielAlonsoVarela@users.noreply.github.com>
2023-10-30 13:34:08 +0100

    Creo adivinanza.py

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,12 @@
+print('¿Qué cosa es, que la hacen cantando, la compran llorando y la usan sin saber?')
+print('a. La escopeta')
+print('b. El ataud')
+print('c. El reloj')
+opcion = input('Selecciona la opcion correcta (a/b/c): ')
+
+if opcion == 'a':
+    print('Opcion Incorrecta')
+elif opcion == 'c':
+    print('Opcion Incorrecta')
+else:
+    print('Opcion Correcta')
