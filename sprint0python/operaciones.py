a6afa41f3928b4a355ae5ea20cd7ebd3b7dc24f6
GabrielAlonsoVarela <145001357+GabrielAlonsoVarela@users.noreply.github.com>
2023-10-30 16:01:59 +0100

    Update operaciones.py

--- sprint0python/operaciones.py
+++ sprint0python/operaciones.py
@@ -1,1 +1,14 @@
+def suma_de_dos_numeros(a, b):
+    return a + b
 
+def resta_de_dos_numeros(a, b):
+    return a - b
+
+def multiplicacion_de_dos_numeros(a, b):
+    return a * b
+
+def division_de_dos_numeros(a, b):
+    if b!=0:
+        return a / b
+    else:
+        print('No se puede dividir entre 0')
