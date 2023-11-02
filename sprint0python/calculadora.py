7987d638e4bfb5a1443ded16dc22a36f44a7b240
GabrielAlonsoVarela <145001357+GabrielAlonsoVarela@users.noreply.github.com>
2023-11-02 10:28:16 +0100

    Updateo calculadora.py añadiendo comentarios

--- sprint0python/calculadora.py
+++ sprint0python/calculadora.py
@@ -1,8 +1,10 @@
+#Importamso las funciones de nuestro archivo operaciones
 from operaciones import suma_de_dos_numeros, resta_de_dos_numeros, multiplicacion_de_dos_numeros, division_de_dos_numeros
 
 continuar = 's'
 print('Calculadora:')
 while continuar == 's':
+    #Pedimos al usuario introducir 2 numeros que guardaremos en variables
     n1 = float(input('Introduce el primer número: '))  # Convertir a float para permitir números con decimales
     n2 = float(input('Introduce el segundo número: '))  # Convertir a float para permitir números con decimales
 
@@ -11,9 +13,11 @@ while continuar == 's':
     2. Resta
     3. Multiplicación
     4. División""")
-    
+
+    #Le pedimos al usuario que elija una operacion y la guardamos en otra variable
     op = input('Seleccione una opción (1, 2, 3 o 4): ')
-    
+
+    #Segun la respuesta del usuario llamaremos a una de las funciones importadas y guardaremos el resultado en una variable
     if op in ('1', '2', '3', '4'):
         if op == '1':
             resultado = suma_de_dos_numeros(n1, n2)
