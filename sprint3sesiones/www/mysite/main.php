3df07871a3e609437f1140e6d169c38aa137fe00
Gabriel Alonso <galonsov22@fpcoruna.afundacion.org>
2023-11-16 13:52:25 +0100

    Tarea 7: Añado fade-in a los elementos de la li de main.php

--- sprint3sesiones/www/mysite/main.php
+++ sprint3sesiones/www/mysite/main.php
@@ -3,39 +3,67 @@ session_start();
 ?>
 <html>
 <head>
-<style>
-img{
-width: 150px;
-height:150px;
-}
-</style>
-<?php
-$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die ('Fail');
-?>
+    <style>
+        img {
+            width: 150px;
+            height: 150px;
+        }
+
+        /* Estilos básicos para la lista */
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            margin: 10px;
+            padding: 10px;
+            background-color: #f0f0f0;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        /* Estilos específicos para la animación de fade-in */
+        .fade-in {
+            opacity: 0;
+            transition: opacity 0.5s ease-in-out;
+        }
+
+        .fade-in:hover {
+            opacity: 1;
+        }
+    </style>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            // Agregar la clase fade-in a los elementos de la lista
+            $('li').addClass('fade-in');
+        });
+    </script>
 </head>
 <body>
-<?php
+    <?php
+    if (isset($_SESSION['usuario_id'])) {
+        echo '<a href="/logout.php">Cerrar sesión</a>';
+    }
 
-if (isset($_SESSION['usuario_id'])) {
-    echo '<a href="/logout.php">Cerrar sesión</a>';
-}
+    $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die ('Fail');
 
+    $query = 'SELECT * FROM tJuegos';
+    $result = mysqli_query($db, $query) or die('Query error');
 
-//lanzar una query
-$query = 'SELECT * FROM tJuegos';
-$result = mysqli_query($db, $query) or die('Query error');
-// Pintamos los datos con un while para que nos muestre todas las filas
-while ($row = mysqli_fetch_array($result)){
-echo '<h1><a href="/detail.php?id='.$row[0].'">'.$row[0].'.'.$row[1].'</a></h1>';
-echo '<img src='.$row[2].'></img>';
-echo '<ul>';
-echo '<li>Precio: '.$row[3].'</li>';
-echo '<li>Plataforma: '.$row[4].'</li>';
-echo '</ul>';
-echo '<hr/>';
-}
+    while ($row = mysqli_fetch_array($result)) {
+        echo '<h1><a href="/detail.php?id=' . $row[0] . '">' . $row[0] . '.' . $row[1] . '</a></h1>';
+        echo '<img src=' . $row[2] . '></img>';
+        echo '<ul>';
+        echo '<li class="fade-in">Precio: ' . $row[3] . '</li>';
+        echo '<li class="fade-in">Plataforma: ' . $row[4] . '</li>';
+        echo '</ul>';
+        echo '<hr/>';
+    }
 
-mysqli_close($db);
-?>
+    mysqli_close($db);
+    ?>
 </body>
 </html>
